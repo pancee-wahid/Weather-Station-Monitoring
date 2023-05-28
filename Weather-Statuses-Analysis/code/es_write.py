d5c7dbd5f6903bedd4ec02293b3bf2a78b3d5b5c
aya-khames <ya443858@gmail.com>
2023-05-28 18:02:54 +0300

    parquet file writer into ES script updates

--- Weather-Statuses-Analysis/code/es_write.py
+++ Weather-Statuses-Analysis/code/es_write.py
@@ -11,24 +11,20 @@ empty_list = []
 for i in range(10):
     done.append(empty_list) 
 
-folders_list = os.listdir('/home/elastic/parquet_files')
-# folders_list = os.listdir('F:\\term-2\Data intensive\labs\Weather-Station-Monitoring\Archiving - Python\parquet-files')
-
+folders_list = os.listdir('parquet_files')
 
 while True:
     for i in range(10):
-        folder_files = os.listdir("/home/elastic/parquet_files/" + folders_list[i])
-        # folder_files = os.listdir("F:\\term-2\Data intensive\labs\Weather-Station-Monitoring\Archiving - Python\parquet-files\\" + folders_list[i])
+        folder_files = os.listdir("/parquet_files/" + folders_list[i])
         for file in folder_files:
             if not (file in done[i]):
-                loc = "/home/elastic/parquet_files/" + folders_list[i] + "/" + file
-                # loc = "F:\\term-2\Data intensive\labs\Weather-Station-Monitoring\Archiving - Python\parquet-files\\" + folders_list[i] + "\\" + file
+                loc = "/parquet_files/" + folders_list[i] + "/" + file
                 data = pd.read_parquet(loc, engine='auto')
                 es_client = connections.create_connection(hosts=["http://localhost:9200/"])
                 def doc_generator(pandas_data):
                     for index, document in pandas_data.iterrows():
                         yield {
-                            "_index": 'status',
+                            "_index": 'status_map',
                             "_source": document.to_dict(),
                         }
                 helpers.bulk(es_client, doc_generator(data))
